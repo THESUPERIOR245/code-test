fdd63a4d3ad668eb22564acf6f3107adf47fd781
THESUPERIOR245 <130232974+THESUPERIOR245@users.noreply.github.com>
2023-12-13 08:48:08 +0800

    Create electricitybill.java

---
+++ electricitybill.java
@@ -0,0 +1,14 @@
+public class electricitybill {
+    public static void main(String args[]) {
+      int first=10;
+      int next = 20;
+      int after = 30;
+
+first = 10 * 0;
+next = 20 * 5;
+after = 30 * 10;
+      System.out.println("first unit is  " + first);
+         System.out.println("next unit is  " + next);
+            System.out.println("after unit is " + after);
+    }
+}
