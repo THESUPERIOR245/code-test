f5bc60cb5dbbc5aa9b99ccb9f162ed4c717cfd10
THESUPERIOR245 <130232974+THESUPERIOR245@users.noreply.github.com>
2023-12-13 08:46:29 +0800

    findthegreatestnumber.java

---
+++ findthegreatestnumber.java
@@ -0,0 +1,23 @@
+public class findthegreatestnumber
+{
+    public static void main(String args[]) {
+      int num1=10;
+      int num2=8;
+      int num3=2;
+
+if(num1>num2 && num1>num3)
+    {
+
+        System.out.println("the greatest number is:" + num1);
+    }
+
+        else if(num2>num1 && num2>num3)
+    {
+        System.out.println("the greatest number is:" + num2);
+    }
+        else
+    {
+        System.out.println("the greatest number is:" + num3);
+    }
+}
+}
