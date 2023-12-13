9f51ca37bf5b3d6597c6c557556a2a30a72f27ba
THESUPERIOR245 <130232974+THESUPERIOR245@users.noreply.github.com>
2023-12-13 08:47:32 +0800

    Create State.java

---
+++ State.java
@@ -0,0 +1,69 @@
+public class State {
+    public static void main(String args[]) {
+      int America = 10000002;
+        int Bangladesh = 2554545;
+         int Canada = 255522;
+        int Dubai = 6446666;
+         int England = 75777757;
+         int Finland =688868;
+         int Germany =7878785;
+         int Hawaii =4433333;
+        int Iceland =1112222;
+         int Japan =111144445;
+        int Kenya =556666;
+    int Laos =22344556;
+    int Mali =454544543;
+    int Nepal =22232111;
+    int Oman =80032;
+    int Pakistan =7788954;
+    int Qatar =3567886;
+    int Russia =74256;
+    int Serbia =876542;
+    int Texas =2244567;
+   
+  
+
+
+      System.out.println("America: " + America);
+
+      System.out.println("Bangladesh: " + Bangladesh);
+     
+      System.out.println("Canada: " + Canada);
+     
+      System.out.println("Dubai: " + Dubai);
+  
+      System.out.println("England: " + England);
+      
+      System.out.println("Finland: " + Finland);
+      
+      System.out.println("Germany: " + Germany);
+     
+      System.out.println("Hawaii: " + Hawaii);
+      
+      System.out.println("Iceland: " + Iceland);
+     
+      System.out.println("Japan: " + Japan);
+   
+      System.out.println("Kenya: " + Kenya);
+      
+      System.out.println("Laos: " + Laos);
+     
+      System.out.println("Mali: " + Mali);
+     
+      System.out.println("Nepal: " + Nepal);
+      
+      System.out.println("Oman: " + Oman);
+      
+      System.out.println("Pakistan: " + Pakistan);
+    
+      System.out.println("Qatar: " + Qatar);
+      
+      System.out.println("Russia: " + Russia);
+      
+      System.out.println("Serbia: " + Serbia);
+      
+      System.out.println("Texas: " + Texas);
+
+
+    }
+}
