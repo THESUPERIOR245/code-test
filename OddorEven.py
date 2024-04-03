42384b8b8e326deb36a9d8aa3c34a11df44dc622
THESUPERIOR245 <130232974+THESUPERIOR245@users.noreply.github.com>
2024-04-03 13:25:42 +0800

    Add files via upload

---
+++ OddorEven.py
@@ -0,0 +1,7 @@
+num=int(input("Please enter the number:"))
+print("The entered number is",num)
+
+if num%2:
+    print("This number is an Odd number")
+else:
+    print("This number is an Even number")
