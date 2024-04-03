0329bdb88fee750f6f08334ed80c9b0ba92c84c4
THESUPERIOR245 <130232974+THESUPERIOR245@users.noreply.github.com>
2024-04-03 13:26:11 +0800

    Add files via upload

---
+++ MilitaryAlphabet.py
@@ -0,0 +1,57 @@
+letter=(input("Please enter the letter:"))
+print("You have entered the letter:",letter)
+
+if letter == 'A' or letter == 'a':
+    print("Alpha")
+elif letter == 'B' or letter == 'b':
+    print("Bravo")
+elif letter == 'C' or letter == 'c':
+    print("Charlie")
+elif letter == 'D' or letter == 'd':
+    print("Delta")
+elif letter == 'E' or letter == 'e':
+    print("Echo")
+elif letter == 'F' or letter == 'f':
+    print("Foxtrot")
+elif letter == 'G' or letter == 'g':
+    print("Golf")
+elif letter == 'H' or letter == 'h':
+    print("Hotel")
+elif letter == 'I' or letter == 'i':
+    print("India")
+elif letter == 'J' or letter == 'j':
+    print("Juliet")
+elif letter == 'K' or letter == 'k':
+    print("Kilo")
+elif letter == 'L' or letter == 'l':
+    print("Lima")
+elif letter == 'M' or letter == 'm':
+    print("Mike")
+elif letter == 'N' or letter == 'n':
+    print("November")
+elif letter == 'O' or letter == 'o':
+    print("Oscar")
+elif letter == 'P' or letter == 'p':
+    print("Papa")
+elif letter == 'Q' or letter == 'q':
+    print("Quebec")
+elif letter == 'R' or letter == 'r':
+    print("Romeo")
+elif letter == 'S' or letter == 's':
+    print("Sierra")
+elif letter == 'T' or letter == 't':
+    print("Tango")
+elif letter == 'U' or letter == 'u':
+    print("Uniform")
+elif letter == 'V' or letter == 'v':
+    print("Victor")
+elif letter == 'W' or letter == 'w':
+    print("Whiskey")
+elif letter == 'X' or letter == 'x':
+    print("X-ray")
+elif letter == 'Y' or letter == 'y':
+    print("Yankee")
+elif letter == 'Z' or letter == 'z':
+    print("Zulu")
+else:
+    print("Invalid input")
